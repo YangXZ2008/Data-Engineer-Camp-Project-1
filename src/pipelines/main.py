1ee6bc310978f8b92b0bd8f76a8b74cab2507d1f
Harry <huyhaduchuy1410@gmail.com>
2023-08-31 21:51:00 +1200

    completed  etl oop

--- src/pipelines/main.py
+++ src/pipelines/main.py
@@ -1,13 +1,10 @@
 import pandas as pd
 from dotenv import load_dotenv
 import os 
-from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData, Float
-from sqlalchemy.engine import URL
-from sqlalchemy.dialects import postgresql
-from sqlalchemy.schema import CreateTable 
+from sqlalchemy import Table, Column, Integer, String, MetaData, Float
 from src.connectors.nsw_api import NswApiClient
 from src.connectors.postgresql import PostgreSqlClient
-from src.assests.nsw_api import extract, transform, load
+from src.assests.nsw_api import extract, transform_2_df, load
 
 
 
@@ -18,8 +15,43 @@ if __name__ == "__main__":
     DB_PASSWORD = os.environ.get("DB_PASSWORD")
     SERVER_NAME = os.environ.get("SERVER_NAME")
     DATABASE_NAME = os.environ.get("DATABASE_NAME")
+    PORT = os.environ.get("PORT")
 
     nsw_api_client = NswApiClient(auth=AUTH, tranid=1)
-    df_prices, df_stations = extract(api_client = nsw_api_client)
+    
+    df_prices, df_stations = extract(api_client = nsw_api_client.extract_nsw_fuel_data())
 
-    df_prices_final, df_stations_final = transform(prices_df=df_prices, stations_df=df_stations )
+    df_prices_final, df_stations_final = transform_2_df(prices_df=df_prices, stations_df=df_stations )
+
+
+    #DB Connection
+    postgresql_client = PostgreSqlClient(
+        server_name=SERVER_NAME,
+        database_name=DATABASE_NAME,
+        username=DB_USERNAME,
+        password=DB_PASSWORD,
+        port=PORT
+    )
+    # create ORM model 
+    meta = MetaData()
+    fuel_stations_tbl = Table(
+        "nsw_stations", meta, 
+        Column("name", String),
+        Column("station_code", Integer),
+        Column("address", String),
+        Column("lat", Float),
+        Column("long", Float)
+    )
+
+    fuel_prices_tbl = Table(
+        "nsw_fuel_price", meta,
+        Column('id', String, primary_key=True),
+        Column("station_code", Integer),
+        Column("fuel_type", String),
+        Column("price", Float),
+        Column("last_updated", String, primary_key=True)
+    )
+
+    load(df=df_stations_final, pg_client=postgresql_client, table=fuel_stations_tbl, metadata=meta, loadtype='normal')
+
+    load(df=df_prices_final, pg_client=postgresql_client, table=fuel_prices_tbl, metadata=meta, loadtype='chunk')
