88e7ff753c3ac154f400c553d86c586295109c24
qqto2008 <99694690+XianZheYang@users.noreply.github.com>
2023-09-06 21:50:57 +1000

    log

--- src/pipelines/fuel_pipeline.py
+++ src/pipelines/fuel_pipeline.py
@@ -1,69 +1,77 @@
-import json
-from pprint import pprint
-import pandas as pd
 from dotenv import load_dotenv
 import os
-from datetime import datetime
 from sqlalchemy import Table, Column, Integer, String, MetaData, Float
 from connectors.fuel_api import FuelAPIClient
 from connectors.postgres_client import PostgreSqlClient
 from assets.fuel_extract import extract, load, transform
-
+from assets.pipeline_logging import PipelineLogging
 
 if __name__ == '__main__':
+    # Initialize the logger
+    logger = PipelineLogging('fuel_pipeline', '../logs')
+    logger.info("Starting the fuel pipeline...")
 
-    load_dotenv()
-    API = os.getenv('APIKEY')
-    APISECRET = os.getenv('APISECRET')
-    AUTHORIZATIONHEADER = os.getenv('AUTHORIZATIONHEADER')
-    DB_USERNAME = os.environ.get("DB_USERNAME")
-    DB_PASSWORD = os.environ.get("DB_PASSWORD")
-    SERVER_NAME = os.environ.get("DB_SERVER_NAME")
-    DATABASE_NAME = os.environ.get("DB_DATABASE_NAME")
-    PORT = os.environ.get("PORT")
+    try:
+        load_dotenv()
+        API = os.getenv('APIKEY')
+        APISECRET = os.getenv('APISECRET')
+        AUTHORIZATIONHEADER = os.getenv('AUTHORIZATIONHEADER')
+        DB_USERNAME = os.environ.get("DB_USERNAME")
+        DB_PASSWORD = os.environ.get("DB_PASSWORD")
+        SERVER_NAME = os.environ.get("DB_SERVER_NAME")
+        DATABASE_NAME = os.environ.get("DB_DATABASE_NAME")
+        PORT = os.environ.get("PORT")
 
-    testAPI = FuelAPIClient(API, APISECRET, AUTHORIZATIONHEADER)
-    data_station, data_fuel = extract(testAPI)
-    df_station = transform(data_station, table="station")
-    df_fuel = transform(data_fuel, table="fuel")
+        logger.info("Loaded environment variables")
 
-    postgresql_client = PostgreSqlClient(
-        server_name=SERVER_NAME,
-        database_name=DATABASE_NAME,
-        username=DB_USERNAME,
-        password=DB_PASSWORD,
-        port=PORT
-    )
+        testAPI = FuelAPIClient(API, APISECRET, AUTHORIZATIONHEADER)
+        data_station, data_fuel = extract(testAPI)
+        df_station = transform(data_station, table="station")
+        df_fuel = transform(data_fuel, table="fuel")
 
-    metadata_station = MetaData()
-    table_station = Table(
-        "station", metadata_station,
-        Column("station_code", Integer, primary_key=True),
-        Column("lat", Float, primary_key=True),
-        Column("lon", Float, primary_key=True),
-        Column("name", String),
-        Column("brand", String),
-        Column("address", String),
-        Column("state", String)
-    )
-    load(df_exchange=df_station, postgresql_client=postgresql_client,
-         table=table_station, metadata=metadata_station)
+        logger.info("Data extraction and transformation completed")
 
-    metadata_fuel = MetaData()
-    table_fuel = Table(
-        "fuel_prices", metadata_fuel,
-        Column("station_code", Integer, primary_key=True),
-        Column("last_updated", String, primary_key=True),
-        Column("fuel_type", String, primary_key=True),
-        Column("price", Float),
-        Column("state", String)
-    )
+        postgresql_client = PostgreSqlClient(
+            server_name=SERVER_NAME,
+            database_name=DATABASE_NAME,
+            username=DB_USERNAME,
+            password=DB_PASSWORD,
+            port=PORT
+        )
 
-    if df_fuel.shape[0] > 5000:
-        load(df_exchange=df_fuel[:5000], postgresql_client=postgresql_client,
-             table=table_fuel, metadata=metadata_fuel)
-        load(df_exchange=df_fuel[5000:], postgresql_client=postgresql_client,
-             table=table_fuel, metadata=metadata_fuel)
-    else:
+        metadata_station = MetaData()
+        table_station = Table(
+            "station", metadata_station,
+            Column("station_code", Integer, primary_key=True),
+            Column("lat", Float, primary_key=True),
+            Column("lon", Float, primary_key=True),
+            Column("name", String),
+            Column("brand", String),
+            Column("address", String),
+            Column("state", String)
+        )
+        metadata_fuel = MetaData()
+        table_fuel = Table(
+            "fuel_prices", metadata_fuel,
+            Column("station_code", Integer, primary_key=True),
+            Column("last_updated", String, primary_key=True),
+            Column("fuel_type", String, primary_key=True),
+            Column("price", Float),
+            Column("state", String)
+        )
+
+        logger.info("Database setup completed")
+
+        load(df_exchange=df_station, postgresql_client=postgresql_client,
+             table=table_station, metadata=metadata_station)
+
         load(df_exchange=df_fuel, postgresql_client=postgresql_client,
              table=table_fuel, metadata=metadata_fuel)
+
+        logger.info("Data loading to the database completed")
+
+    except Exception as e:
+        # Log any exceptions that occur
+        logger.error(f"An error occurred: {str(e)}")
+
+    logger.info("Fuel pipeline finished.")
