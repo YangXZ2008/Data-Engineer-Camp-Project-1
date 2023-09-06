8694e1e7c4469b7063d2046221007f71e44f543c
qqto2008 <99694690+XianZheYang@users.noreply.github.com>
2023-09-06 21:18:13 +1000

    log

--- src/pipelines/fuel_pipeline.py
+++ src/pipelines/fuel_pipeline.py
@@ -1,28 +1,31 @@
+import json
+from pprint import pprint
+import pandas as pd
 from dotenv import load_dotenv
 import os
+from datetime import datetime
 from sqlalchemy import Table, Column, Integer, String, MetaData, Float
 from connectors.fuel_api import FuelAPIClient
 from connectors.postgres_client import PostgreSqlClient
 from assets.fuel_extract import extract, load, transform
 
 
-def get_token():
+if __name__ == '__main__':
+
     load_dotenv()
     API = os.getenv('APIKEY')
     APISECRET = os.getenv('APISECRET')
     AUTHORIZATIONHEADER = os.getenv('AUTHORIZATIONHEADER')
-    testAPI = FuelAPIClient(API, APISECRET, AUTHORIZATIONHEADER)
-
-    return testAPI
-
-
-def get_postgresql_client():
-    load_dotenv()
     DB_USERNAME = os.environ.get("DB_USERNAME")
     DB_PASSWORD = os.environ.get("DB_PASSWORD")
     SERVER_NAME = os.environ.get("DB_SERVER_NAME")
     DATABASE_NAME = os.environ.get("DB_DATABASE_NAME")
     PORT = os.environ.get("PORT")
+
+    testAPI = FuelAPIClient(API, APISECRET, AUTHORIZATIONHEADER)
+    data_station, data_fuel = extract(testAPI)
+    df_station = transform(data_station, table="station")
+    df_fuel = transform(data_fuel, table="fuel")
 
     postgresql_client = PostgreSqlClient(
         server_name=SERVER_NAME,
@@ -31,14 +34,7 @@ def get_postgresql_client():
         password=DB_PASSWORD,
         port=PORT
     )
-    return postgresql_client
 
-
-if __name__ == '__main__':
-
-    data_station, data_fuel = extract(get_token())
-    df_station = transform(data_station, table="station")
-    df_fuel = transform(data_fuel, table="fuel")
     metadata_station = MetaData()
     table_station = Table(
         "station", metadata_station,
@@ -50,8 +46,8 @@ if __name__ == '__main__':
         Column("address", String),
         Column("state", String)
     )
-    load(df_exchange=df_station, postgresql_client=get_postgresql_client(),
-         table=table_station, metadata=metadata_station)
+    load(df_exchange=df_station, postgresql_client=postgresql_client,
+         table=table_station)
 
     metadata_fuel = MetaData()
     table_fuel = Table(
@@ -63,5 +59,5 @@ if __name__ == '__main__':
         Column("state", String)
     )
 
-    load(df_exchange=df_fuel, postgresql_client=get_postgresql_client(),
-         table=table_fuel, metadata=metadata_fuel)
+    load(df_exchange=df_fuel, postgresql_client=postgresql_client,
+         table=table_fuel)
