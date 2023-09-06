5675decf2b97abe056e52f6a4fef791649448903
qqto2008 <99694690+XianZheYang@users.noreply.github.com>
2023-09-06 14:35:20 +1000

    scheduling

--- src/pipelines/fuel_pipeline.py
+++ src/pipelines/fuel_pipeline.py
@@ -14,18 +14,18 @@ def load_config():
         "API": os.getenv('APIKEY'),
         "APISECRET": os.getenv('APISECRET'),
         "AUTHORIZATIONHEADER": os.getenv('AUTHORIZATIONHEADER'),
-        "DB_USERNAME": os.environ.get("DB_USERNAME"),
-        "DB_PASSWORD": os.environ.get("DB_PASSWORD"),
-        "SERVER_NAME": os.environ.get("DB_SERVER_NAME"),
-        "DATABASE_NAME": os.environ.get("DB_DATABASE_NAME"),
-        "PORT": os.environ.get("PORT"),
+        "DB_USERNAME": os.getenv("DB_USERNAME"),
+        "DB_PASSWORD": os.getenv("DB_PASSWORD"),
+        "SERVER_NAME": os.getenv("DB_SERVER_NAME"),
+        "DATABASE_NAME": os.getenv("DB_DATABASE_NAME"),
+        "PORT": os.getenv("PORT"),
     }
 
 
 def create_database_tables():
-    metadata_station = MetaData()
+    metadata = MetaData()
     table_station = Table(
-        "station", metadata_station,
+        "station", metadata,
         Column("station_code", Integer, primary_key=True),
         Column("lat", Float, primary_key=True),
         Column("lon", Float, primary_key=True),
@@ -34,9 +34,8 @@ def create_database_tables():
         Column("address", String),
         Column("state", String)
     )
-    metadata_fuel = MetaData()
     table_fuel = Table(
-        "fuel_prices", metadata_fuel,
+        "fuel_prices", metadata,
         Column("station_code", Integer, primary_key=True),
         Column("last_updated", String, primary_key=True),
         Column("fuel_type", String, primary_key=True),
@@ -63,15 +62,14 @@ def main():
     )
 
     table_station, table_fuel = create_database_tables()
-
-    metadata_station = MetaData()
-    metadata_fuel = MetaData()
+    metadata = MetaData()
 
     try:
         schedule.every(12).hours.do(load, df_exchange=df_fuel, postgresql_client=postgresql_client,
-                                    table=table_fuel, metadata=metadata_fuel)
+                                    table=table_fuel, metadata=metadata)
         schedule.every(12).hours.do(load, df_exchange=df_station, postgresql_client=postgresql_client,
-                                    table=table_station, metadata=metadata_station)
+                                    table=table_station, metadata=metadata)
+
         while True:
             schedule.run_pending()
             time.sleep(1)
