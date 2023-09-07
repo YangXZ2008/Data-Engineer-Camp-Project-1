9c0b0dcfbe8a927e193b9e3b7a50161ec1f36cb8
qqto2008 <99694690+XianZheYang@users.noreply.github.com>
2023-09-07 10:47:20 +1000

    added metalog and transform

---
+++ src/pipelines/sql.transform.py
@@ -0,0 +1,22 @@
+from jinja2 import Environment
+from connectors.postgresql import PostgreSqlClient
+
+
+class SqlTransform:
+    def __init__(self, postgresql_client: PostgreSqlClient, environment: Environment, table_name: str):
+        self.postgresql_client = postgresql_client
+        self.environment = environment
+        self.table_name = table_name
+        self.template = self.environment.get_template(f"{table_name}.sql")
+
+    def create_table_as(self) -> None:
+        """
+        Drops the table if it exists and creates a new copy of the table using the provided select statement. 
+        """
+        exec_sql = f"""
+            drop table if exists {self.table_name};
+            create table {self.table_name} as (
+                {self.template.render()}
+            )
+        """
+        self.postgresql_client.execute_sql(exec_sql)
